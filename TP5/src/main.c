3577b07d56af69fb23d590eaaf192207c6fbd0ea
Metro-Lun <mehdi.bourbon@gmail.com>
2025-12-31 12:54:56 +0100

    5.1 quasi fini

--- TP5/src/main.c
+++ TP5/src/main.c
@@ -71,51 +71,70 @@ Variable* find_variable(Variable** variables, int* size, char* name) {
     return NULL;
 }
 
+char* get_type(char** string_value) {
+    if(strcspn(*string_value, "_\"\'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") != strlen(*string_value))
+        return "str";
+    else if(strcspn(*string_value, ".,") == strlen(*string_value))
+        return "int";
+    else if(strcspn(*string_value, ".,") != strlen(*string_value))
+        return "float";
+    else
+        return "other";
+}
+
 /**
  * Cette fonction detecte si l'utilisateur a entre une variable.
  * 
  * Codes de sortie: 
- * 0 - succes, pas d'erreur
- * 1 - n'est pas une variable
- * 2 - assignation erronnee : il faudrait trois champs
- * 3 - token operande invalide
- * 4 - token operateur invalide
- * 5 - erreur realloc
+ * 0 - succes (traitement postérieur non nécessaire)
+ * 1 - succes (désormais il faut traiter l'opération)
+ * 2 - erreur, voir retour à l'écran
  */
-int check_variable(char** input, Variable** variables, int* size) {
+int check_variable(char** input, Variable** variables, int* size) {  
     if(strcspn(*input, "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") == strlen(*input)) {
-        return 1; // n'est pas une variable
+        return 1; // n'est pas une variable donc tout va bien
     }
 
-    if(strcspn(*input, "=") != strlen(*input))
-    { // assignation
-        char* token = strtok(*input, " ");
-        int input_length = 0;
-        while(token != NULL) {
-            input_length++;
-            token = strtok(NULL, " ");
-        }
-        if(input_length != 3) return 2;
+    char* input_copy = strdup(*input);
+    char* token = strtok(input_copy, " ");
+    int input_length = 0;
+    while(token != NULL) {
+        input_length++;
+        token = strtok(NULL, " ");
+    }
+
+    // assignation
+    if(strcspn(*input, "=") != strlen(*input)) {
+        // TODO: cas de figure où le string possède des espaces
+
+        if(input_length != 3)
+            return 3;
 
         token = strtok(*input, " ");
         Variable var_to_add;
         int index = 0;
 
         while(token != NULL) {
+            printf("-----> %s\n", token);
             switch(index) {
                 case 0 :
-                    if(isdigit(token[0])) return 3;
-                    var_to_add.name = token;
+                    if(isdigit(token[0])) {
+                        printf("Erreur: le nom d'une variable ne doit pas commencer par un nombre\n");
+                        return 4;
+                    }
+                    var_to_add.name = strdup(token);
                     break;
                 case 1 :
-                    if(strcmp(token, "=") != 0) return 4;
+                    if(strcmp(token, "=") != 0)
+                        return 5;
                     break;
-                case 2 :
-                    if(strcspn(token, ".,") == strlen(token)) var_to_add.type = "int";
-                    else if(strcspn(token, ".,") != strlen(token)) var_to_add.type = "float";
-                    else if(strcspn(token, "_\"\'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") != strlen(token)) var_to_add.type = "str"; //TODO: verifier balises d'ouverture et de fermeture
-                    else return 3;
-                    var_to_add.value = token;
+                case 2 : // TODO: erreur de string : ne capte que les 3 premiers caractères
+                    char* new_type = get_type(&token);
+                    if(strcmp(new_type, "other") == 0)
+                        return 4;
+                    else
+                        var_to_add.type = new_type;
+                    var_to_add.value = strdup(token);
                     break;
                 default:
                     break;
@@ -132,40 +151,65 @@ int check_variable(char** input, Variable** variables, int* size) {
             Variable* new_variables = realloc(*variables, sizeof(Variable) * (*size));
             if(variables == NULL) {
                 perror("realloc");
-                return 5;
+                return 6;
             } else {
                 new_variables[*size-1] = var_to_add;
                 *variables = new_variables;
             }
 
-            printf("%s\n", var_to_add.name);
+            printf("Variable %s définie avec la valeur %s (%s)\n", var_to_add.name, var_to_add.value, var_to_add.type);
+        } 
+        else {
+            char* var_type = get_type(&var_to_add.value);
+            if(strcmp(var_type, "other") == 0 || strcmp(var_type, existing_var->type) != 0) {
+                printf("Erreur: %s est de type %s\n", existing_var->name, existing_var->type);
+                return 4;
+            }
+            
+            existing_var->value = strdup(var_to_add.value);
+            printf("Variable %s redéfinie avec la valeur %s (%s)\n", existing_var->name, var_to_add.value, existing_var->type);
         }
-
-        free(existing_var);
+        return 0;
     }
 
-    else
-    { // remplacer le nom de la variable par sa valeur
-        char* token = strtok(*input, " ");
-        char new_buffer[256];
-        int new_buffer_index = 0;
+    else { // remplacer le nom de la variable par sa valeur
+        char* input_copy = strdup(*input);
+        char* token = strtok(input_copy, " ");
+        char* new_buffer = malloc(1024);
+        new_buffer[0] = '\0';
 
         while(token != NULL) {
-            char* token_to_add;
-            if(strcspn(token, "_\"\'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") != strlen(token)) {
-                Variable* var = find_variable(variables, size, token);
-            } else {
-                token_to_add = token;
-            }
+            char* token_to_add = token;
 
-            // TODO: new_buffer += token_to_add;
+            if(strcspn(token, "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") != strlen(token)) {
+                Variable* var = find_variable(variables, size, token);
+                if(var != NULL) {
+                    // pour print uniquement un string
+                    if(input_length == 1 && strcmp(var->type, "str") == 0) {
+                        printf("%s\n", var->value);
+                        return 0;
+                    }
+                    token_to_add = var->value;
+                } else {
+                    printf("Erreur: la variable %s n'est pas définie\n", token);
+                    return 2;
+                }
+            }
+            
+            strcat(new_buffer, token_to_add);
+            strcat(new_buffer, " ");
+
             token = strtok(NULL, " ");
         }
+
+        *input = strdup(new_buffer); // TODO: ajouter difficulté dans le README
+        return 1;
     }
 
-    return 1;
+    return 2;
 }
 
+// TODO: modifier les codes d'erreur
 int main() {
     printf("╔══════════════════════════════════════════╗\n");
     printf("║  ÉVALUATEUR D'EXPRESSIONS ARITHMÉTIQUES  ║\n");
@@ -194,40 +238,43 @@ int main() {
         char* postfix = NULL;
         int need_free = 0;
 
-        // Détecter s'il s'agit d'une variable TODO:
-        check_variable(&input, &variables, &size); // TODO: ajouter pointeurs vers la structure Variables et input
-        
-        // Détecter si l'expression est déjà en postfixe
-        if(is_postfix_expression(input)) {
-            printf("-> Expression détectée: POSTFIXE\n");
-            postfix = input; // Utiliser directement l'input
-        } else {
-            printf("-> Expression détectée: INFIXE\n");
-            // Conversion de l'infixé au postfixé
-            postfix = infix_to_postfix(input);
-            need_free = 1;
-            
-            if(postfix == NULL) {
-                printf("Erreur: Expression invalide (problème de syntaxe ou de parenthèses)\n");
-                continue;
+        int check = check_variable(&input, &variables, &size);
+        if(check == 1) {
+            // Détecter si l'expression est déjà en postfixe
+            if(is_postfix_expression(input)) {
+                // printf("-> Expression détectée: POSTFIXE\n");
+                postfix = input; // Utiliser directement l'input
+            } else {
+                // printf("-> Expression détectée: INFIXE\n");
+                // Conversion de l'infixé au postfixé
+                postfix = infix_to_postfix(input);
+                need_free = 1;
+                
+                if(postfix == NULL) {
+                    printf("Erreur: Expression invalide (problème de syntaxe ou de parenthèses)\n");
+                    continue;
+                }
+                
+                // printf("-> Expression postfixée: '%s'\n", postfix);
             }
             
-            printf("-> Expression postfixée: '%s'\n", postfix);
+            // Évaluation de l'expression postfixée
+            double result;
+            if(evaluate_postfix(postfix, &result)) {
+                // printf("Résultat: %.2f\n", result);
+                printf("%.2f\n", result);
+            } else {
+                printf("Erreur: Évaluation impossible (opérande(s) manquant(s) ou division par zéro)\n");
+            }
+            
+            if(need_free) {
+                free(postfix);
+            }
         }
+
         
-        // Évaluation de l'expression postfixée
-        double result;
-        if(evaluate_postfix(postfix, &result)) {
-            printf("Résultat: %.2f\n", result);
-        } else {
-            printf("Erreur: Évaluation impossible (opérande(s) manquant(s) ou division par zéro)\n");
-        }
-        
-        if(need_free) {
-            free(postfix);
-        }
     }
     
-    printf("\nAu revoir!\n");
+    printf("\nAu revoir !\n");
     return 0;
 }
