ccdb42471cea4d5047f1332597dc4e1140dd9002
Metro-Lun <mehdi.bourbon@gmail.com>
2026-01-01 19:21:34 +0100

    5.2 completed

--- TP5/src/main.c
+++ TP5/src/main.c
@@ -371,7 +371,6 @@ int main() {
 
         int ck_lambda = check_lambda(&input, &variables, &size);
         if(ck_lambda == 1) {
-            printf("dek,ded\n");
             continue;
         }
 
